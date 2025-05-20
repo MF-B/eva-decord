7a6df3ece006abdb71373b573367d34c8805881a
mf1bzz-desktop <guzemin@hotmail.com>
2025-05-21 00:00:35 +0800

    suit for openeuler

--- src/video/ffmpeg/ffmpeg_common.h
+++ src/video/ffmpeg/ffmpeg_common.h
@@ -21,9 +21,7 @@
 extern "C" {
 #endif
 #include <libavcodec/avcodec.h>
-#ifdef __APPLE__
-    #include <libavcodec/bsf.h>
-#endif
+#include <libavcodec/bsf.h>
 #include <libavformat/avformat.h>
 #include <libavformat/avio.h>
 #include <libavfilter/avfilter.h>
